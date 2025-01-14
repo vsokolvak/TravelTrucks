9670416746f48bdad08cd73d5123bd484e300ad6
Vadim <vsokolvak2000@gmail.com>
2025-01-14 14:46:21 +0200

    зверстав домашню сторінку

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,9 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import style from './Style.module.css'
 
 function HomePage() {
-  return <div className={style.container}>
+  const nav = useNavigate();
+  const goToHome = () => {
+    nav('/catalog')
+  }
 
-  </div>;
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <h1 className={style.contentTitle}>Campers of your dreams</h1>
+        <p className={style.contentText}>
+          You can find everything you want in our catalog
+        </p>
+        <button onClick={goToHome} className={style.contentLink}>
+          View Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
